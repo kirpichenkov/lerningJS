0625ecdde2f03121c8b7ab36e2ce6a32826728eb
Kirpichenkov Aleksandr <kirpe4enkov@gmail.com>
2023-11-16 19:43:36 +0300

    add less8, remembered for/while/do dwhile

---
+++ JS/petrichenko/less7/less 7.js
@@ -0,0 +1,10 @@
+var num = 17;
+switch (num){
+    case 15:
+        console.log("Верно");
+        break;
+    default:
+        console.log("ты ошибся");
+        break;
+        
+}
---
+++ JS/petrichenko/less8/less8.js
@@ -0,0 +1,10 @@
+/*
+Есть 3 основных типа циклов
+1. while
+*/
+var num = 50;
+
+while(num < 55){
+    console.log(num);
+    num++;
+};
